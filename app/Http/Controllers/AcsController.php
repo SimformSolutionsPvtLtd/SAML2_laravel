2c83253c64735de24b7f36fd85297dc4a8e76a1d
mritunjay khichi <mritunjay.k@simformsolutions.com>
2023-02-06 12:59:57 +0530

    return type declaration

--- app/Http/Controllers/AcsController.php
+++ app/Http/Controllers/AcsController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AcsController extends Controller
 {
-    public function samlResponse(Request $request)
+    public function samlResponse(Request $request):Response
     {
         return response(base64_decode($request->SAMLResponse), 200)->header('Content-Type', 'application/xml');
     }
